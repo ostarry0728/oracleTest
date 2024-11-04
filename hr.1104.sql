366c1f4aa5582f568ef00a587dbebcb400a056a0
Oh SeungTaek <ostarry@gmail.com>
2024-11-04 16:16:17 +0900

    distinct, ||, as, select 테스트

--- hr.1104.sql
+++ hr.1104.sql
@@ -2,4 +2,23 @@
 select * from tab;
 --employees 테이블 구조
 desc employees;
--- employees 속에 들어있는 레코드 확인 
+-- employees 속에 들어있는 레코드 확인
+select * from employees;
+-- departments 테이블 객체(레코드=인스턴스)를 확인
+select * from departments;
+-- departments 구조
+desc depertments;
+-- department_id, department_name 보임
+select department_id, department_name from departments;
+--필드명에 별칭사용하기
+select department_id as "부서번호", department_name as "부서명" from departments;
+select department_id as DEPT_NO, department_name as DEPT_NAME from departments;
+select department_id as "DEPT NO", department_name as "DEPT NAME" from departments;
+-- + ||
+select '5' + 5 from dual;
+select '5' || 5 from dual;
+-- 문자열을 기능을 이용해서 필드명을 보여주기
+select first_name, job_id from employees;
+select first_name || '의 직급은' || job_id || '입니다' as data from employees;
+-- 중복되지 않게 보여주기 distinct
+select distinct job_id from employees;
