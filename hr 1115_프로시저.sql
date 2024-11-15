55fcad20275de0d59a25bc5f950f013635fc647e
Oh SeungTaek <ostarry@gmail.com>
2024-11-15 14:25:38 +0900

    부서별로 월급을 인상하는 프로그램 작성. 10번부서 전원 10% 인상, 20번부서는 전원 20% 인상, 나머지는 동결. 테스트 완료

--- hr 1115_프로시저.sql
+++ hr 1115_프로시저.sql
@@ -1,14 +1,117 @@
-create or replace NONEDITIONABLE PROCEDURE EMP_PROC2 (VWORD IN VARCHAR2)
-IS  
-    
-    VEMP_ROWTYPE EMPLOYEES%ROWTYPE;
-    CURSOR CURSOR_NAME(VWORD VARCHAR2)
-    IS
-    SELECT * FROM EMPLOYEES WHERE UPPER(FIRST_NAME) LIKE UPPER('%'||VWORD||'%');
+-- EMP01 테이블 복사
+
+SELECT * FROM USER_TABLES WHERE TABLE_NAME = 'EMP01';
+DROP TABLE EMP01;
+CREATE TABLE EMP01
+AS
+SELECT  * FROM EMPLOYEES WHERE 1 = 1;
+SELECT * FROM EMP01;
+
+-- 부서별로 월급을 인상하는 프로그램이다. 10번부서 전원 10% 인상, 20번부서는 전원 20% 인상, 나머지는 동결.
+CREATE OR REPLACE PROCEDURE EMP01_PROC(VDEPTNO IN EMPLOYEES.DEPARTMENT_ID%TYPE)
+IS
 BEGIN
-    FOR VEMP_ROWTYPE IN CURSOR_NAME(VWORD)LOOP
-        DBMS_OUTPUT.PUT_LINE(VEMP_ROWTYPE.FIRST_NAME || '/' || VEMP_ROWTYPE.SALARY);
-    END LOOP;
+    IF VDEPTNO = 10 THEN 
+        UPDATE EMP01 SET SALARY = SALARY * 1.1 WHERE DEPARTMENT_ID = VDEPTNO;
+        DBMS_OUTPUT.PUT_LINE(VDEPTNO ||'부서는 전원 10% 인상했습니다.');
+    ELSIF VDEPTNO = 20 THEN
+        UPDATE EMP01 SET SALARY = SALARY * 1.2 WHERE DEPARTMENT_ID = VDEPTNO;
+        DBMS_OUTPUT.PUT_LINE(VDEPTNO ||'부서는 전원 20% 인상했습니다.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(VDEPTNO ||'부서는 전원동결입니다.');
+    END IF;
 END;
 /
-EXECUTE EMP_PROC2('K'};                                                         
+
+SELECT * FROM EMP01 WHERE DEPARTMENT_ID = 10;
+
+EXECUTE EMP01 PROC(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
